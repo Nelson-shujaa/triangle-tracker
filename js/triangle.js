52cdf1a8896c2eb13f418fe7e5dbd5bf171a57eb
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-02-15 14:11:06 +0300

    changes to include files

--- js/triangle.js
+++ js/triangle.js
@@ -1,7 +1,18 @@
-alert("Welcome to robo calculator");
-var a = parseInt(prompt("Enter a number"));
-var b = parseInt(prompt("Enter a second number"));
-var c = parseInt(prompt("Enter a third number"));
- if (a+b=c || a+b>c) {
-   alert("This is not a triangle\n Please try numbers that fit the criteria: a + b >=");
- }
+function myFunction() {
+  alert("Welcome to robo calculator");
+  var a = document.getElementsByName("")
+  var b = parseInt(prompt("Enter a second number"));
+  var c = parseInt(prompt("Enter a third number"));
+   if ((a+b)<=c || (a+c)<=b || (b+c)<=a) {
+    alert("This is not a triangle\n Please try numbers that fit the criteria: a + b >=c");
+   }
+  else if (a==b || b==c || a==c) {
+    alert("This is an isoceles triangle.");
+  }
+  else if (a==b==c) {
+    alert("This is an equilateral triangle.");
+
+  }
+  else if (a!=b && b!=c && a!=c){
+    alert("This is a scalene triangle.")
+}
