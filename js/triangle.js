d31ad4c91a9ffdc020c4c0b3fdc68e29e30aca47
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-02-15 12:33:21 +0300

    Changes to include triangle changes

---
+++ js/triangle.js
@@ -0,0 +1,7 @@
+alert("Welcome to robo calculator");
+var a = parseInt(prompt("Enter a number"));
+var b = parseInt(prompt("Enter a second number"));
+var c = parseInt(prompt("Enter a third number"));
+ if (a+b=c || a+b>c) {
+   alert("This is not a triangle\n Please try numbers that fit the criteria: a + b >=");
+ }
