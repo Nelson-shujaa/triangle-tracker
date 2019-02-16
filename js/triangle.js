be3680d25696c0cfbee360439557cb5e01b78dfc
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-02-16 10:52:40 +0300

    changes to include right angled triangle

--- js/triangle.js
+++ js/triangle.js
@@ -30,13 +30,17 @@ function myFunction() {
   var a = document.formx.firstvalue.value;
   var b = document.formx.secondvalue.value;
   var c = document.formx.lastvalue.value;
-   if (a+b>c && a+c>b && b+c>a) {
+   if (a+b>c || a+c>b || b+c>a) {
      if (a==b && b==c && a==c) {
        alert("This is an equilateral triangle.");
    }
-   else if (a!=b && b!=c && a!=c){
+    else if (a!=b && b!=c && a!=c){
      alert("This is a scalene triangle.")
   }
+    else if ((a*a) + (b*b) == c*c) {
+     alert("This is a right angled triangle")
+
+   }
   else{
   alert("This is an isoceles triangle.")
   }
